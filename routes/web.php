f247bbd55c18c1790b2d1eeb794256d3cf0a6e7b
12saurav <12saurav@users.noreply.github.com>
2021-03-24 16:06:07 +0530

    5456
    879

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('admin');
 });
 Route::get('admin','Controller@loginForm');
 Route::get('admin/register','Controller@reg');
