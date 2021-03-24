c6ccd32b6965e8e2c67130bb7e351ec04ce7e9f2
12saurav <12saurav@users.noreply.github.com>
2021-03-24 12:59:05 +0530

    24/03/2021
    backend

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,13 +1,51 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\User;
+use Hash;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function loginForm(){
+        $user=User::count();
+        return view('backend.login',['totaladmin'=>$user]);
+    }
+    public function reg(){
+        $user=User::count();
+        if($user==0){
+
+            return view('backend.register');
+        }else{
+            return redirect('admin');
+        }
+    }
+    public function regForm(Request $request){
+
+        $var=$request->input();
+        $request->validate([
+            'name'=>'required | min:4 | max:50',
+            'username'=>'required | email',
+            'password'=>'required |confirmed | min:4 | max:15'
+        ]);
+
+
+        $user= new user;
+        $user->name=$var['name'];
+        $user->email=$var['username'];
+        $user->password=Hash::make($var['password']);
+        $check= $user->save();
+        if($check){
+
+
+          return redirect('admin')->with('data','*Register succesfuly');
+
+        }
+    }
 }
